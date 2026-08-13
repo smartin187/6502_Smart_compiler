d76f2be5f152e17d38bec935478310f177a64f04
sunti <sunti.martin@gmail.com>
2026-08-13 15:03:19 +0200

    docs: change version of smart

--- compiller_tool/smart_info.py
+++ compiller_tool/smart_info.py
@@ -8,7 +8,7 @@ from compiller_tool.color_tool import Colors
 
 FROZEN = getattr(sys, 'frozen', False)  # if the programme is compiled by Pyinstaller
 
-SMART_VERSION = "v0.0.6"
+SMART_VERSION = "v0.0.7"
 
 ONE_LINUX = sys.platform == "linux"
 
