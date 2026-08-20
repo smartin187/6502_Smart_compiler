080038ed0408864287bbf01f44544cb10cd1e6cc
Smartin187 <116507208+smartin187@users.noreply.github.com>
2026-08-20 17:21:42 +0200

    refactor: change build system

--- build_releases/build_linux/build_other_architecture.bash
+++ build_releases/build_linux/build_other_architecture.bash
@@ -3,6 +3,8 @@
 # This script compile and package for the architecture arm64 or amd64.
 # If your computer is amd64, this script build for arm64, if you computer is arm64, this script build for amd64.
 
+# RUN THIS COMMAND BEFORE : sudo docker run --privileged --rm tonistiigi/binfmt --install amd64,arm64
+
 ARCH=$(dpkg --print-architecture)
 
 echo "--- Building for architecture: $ARCH ---"
@@ -13,9 +15,10 @@ if [ "$ARCH" = "arm64" ]; then   # make the executable binary for amd64
         -v "$PWD":/src \
         -w /src \
         python:3.12-bookworm \
-        sh -lc '
-            python -m pip install --no-cache-dir pyinstaller &&
-            sh ./build_releases/build_linux/build_linux.bash
+        /bin/bash -lc '
+            set -eux
+            python -m pip install pyinstaller
+            bash ./build_releases/build_linux/build_linux.bash
         '
 
 elif [ "$ARCH" = "amd64" ]; then   # make the executable binary for arm64
@@ -24,9 +27,10 @@ elif [ "$ARCH" = "amd64" ]; then   # make the executable binary for arm64
         -v "$PWD":/src \
         -w /src \
         python:3.12-bookworm \
-        sh -lc '
-            python -m pip install --no-cache-dir pyinstaller &&
-            sh ./build_releases/build_linux/build_linux.bash
+        /bin/bash -lc '
+            set -eux
+            python -m pip install pyinstaller
+            bash ./build_releases/build_linux/build_linux.bash
         '
 else
     echo "Unsupported architecture: $ARCH, skipping build for other architecture..."
