fd5a53ec01dc2c454680a41bea5f5ef7e6294fb5
Smartin187 <116507208+smartin187@users.noreply.github.com>
2026-08-20 14:25:27 +0200

    feat: add compillation for amd64 from arm64 and for arm64 from amd64, but unstable

--- build_releases/build_linux/build_all_linux.bash
+++ build_releases/build_linux/build_all_linux.bash
@@ -4,3 +4,5 @@
 
 ./build_releases/build_linux/build_linux.bash   # make the executable binary
 ./build_releases/build_linux/build_deb.bash     # make the deb package
+
+./build_releases/build_linux/build_other_architecture.bash   # make the executable binary for the other architecture
---
+++ build_releases/build_linux/build_other_architecture.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script compile and package for the architecture arm64 or amd64.
+# If your computer is amd64, this script build for arm64, if you computer is arm64, this script build for amd64.
+
+ARCH=$(dpkg --print-architecture)
+
+echo "--- Building for architecture: $ARCH ---"
+
+if [ "$ARCH" = "arm64" ]; then   # make the executable binary for amd64
+    sudo docker run --rm \
+        --platform linux/amd64 \
+        -v "$PWD":/src \
+        -w /src \
+        python:3.12-bookworm \
+        sh -lc '
+            python -m pip install --no-cache-dir pyinstaller &&
+            sh ./build_releases/build_linux/build_linux.bash
+        '
+
+elif [ "$ARCH" = "amd64" ]; then   # make the executable binary for arm64
+    sudo docker run --rm \
+        --platform linux/arm64 \
+        -v "$PWD":/src \
+        -w /src \
+        python:3.12-bookworm \
+        sh -lc '
+            python -m pip install --no-cache-dir pyinstaller &&
+            sh ./build_releases/build_linux/build_linux.bash
+        '
+else
+    echo "Unsupported architecture: $ARCH, skipping build for other architecture..."
+fi
+
