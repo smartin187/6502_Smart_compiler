c5176f77098ee0be91d925c9065fa622212a184c
Smartin187 <116507208+smartin187@users.noreply.github.com>
2026-08-29 17:41:40 +0200

    refactor: extract zip creation into dedicated script
    - move zip packaging logic from build_linux.bash to build_zip.bash
    - update build_all_linux.bash to use new script with proper tag and architecture

--- build_releases/build_linux/build_all_linux.bash
+++ build_releases/build_linux/build_all_linux.bash
@@ -2,7 +2,11 @@
 
 # This script compile and package all for Linux.
 
+MAIN_ARCH_DIR_DIST=$(dpkg --print-architecture)
+MAIN_TAG="Linux-${ARCH_DIR_DIST}"
+
 ./build_releases/build_linux/build_linux.bash   # make the executable binary
 ./build_releases/build_linux/build_deb.bash     # make the deb package
+./build_releases/build_linux/build_zip.bash ${MAIN_TAG} ${MAIN_ARCH_DIR_DIST}     # make the zip file
 
 ./build_releases/build_linux/build_other_architecture.bash   # make the executable binary for the other architecture
